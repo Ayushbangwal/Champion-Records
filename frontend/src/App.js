35fb593f568a006cf67db9c9789b9555adbc34ee
Ayush <ayushbangwal0@gmail.com>
2026-03-12 01:00:26 +0530

    added splash screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,17 @@ import SearchResults from './pages/SearchResults';
 const queryClient = new QueryClient();
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+}, []);
+
+if (loading) {
+  return <SplashScreen />;
+}
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
